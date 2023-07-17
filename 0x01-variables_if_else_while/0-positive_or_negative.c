578e1b7d0fb2b93d86d4b484ed6e9ac567d4ca3b
clilexie <clilexie2020@gmail.com>
2023-07-17 21:44:22 +0300

    The last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,28 @@
-nclude <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Positive anything is better than negative nothing
+ * Return 0 (Always)
+ */
 int main(void)
 {
- int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("%d is ", n);
-if (n > 0)
-printf("positive");
-else if (n == 0)
-printf("zero");
-else
-printf("negative");
-printf("\n");
-return (0);
+	int n;
+
+	srand(time(0));
+	n=rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5 )
+	{
+		printf("Last digit of %d is %d and greater than 5\n", n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+				n, n % 10);
+	}
+	else 
+	{
+		printf("Last digit of %d is %d and is 0\n",
+				n, n % 10);
+	}
+	return (0);
 }
