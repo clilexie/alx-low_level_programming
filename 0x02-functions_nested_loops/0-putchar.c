32ca3aa473a60b11257c1c96e2a1d2d8e76e251a
clilexie <clilexie2020@gmail.com>
2023-07-18 18:57:18 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
-#include <main.h>
+#include "main.h"
 /**
- * main - Print_putchar
+ * main - print _putchar
  * is written by mbah
- * Return: Always 0 (success)
+ * Return: 0
  */
+
 int main(void)
 {
-	_puthchar('_')
-	_putchar('p')
-	_puthchar('u')
-	_putchar('t')
-	_putchar('c')
-	_putchar('h')
-	_putchar('a')
-	_putchar('r')
-	_putchar('\n')
-	return (0)
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
