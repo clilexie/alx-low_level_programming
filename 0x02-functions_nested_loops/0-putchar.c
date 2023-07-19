73f7d3713ddf1e47f4ddbe7fb8cab09e705dba59
clilexie <clilexie2020@gmail.com>
2023-07-18 18:49:40 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - Print_putchar
+ * is written by mbah
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	_puthchar('_')
+	_putchar('p')
+	_puthchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+	_putchar('\n')
+	return (0)
+}
