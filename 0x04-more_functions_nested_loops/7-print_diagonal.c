6158b2dea1c32575e05d16becbfe85efcbba3fd5
clilexie <clilexie2020@gmail.com>
2023-07-20 12:38:18 -0700

    diagonol

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+# include "main.h"
+
+/**
+ *print_diagonal - printing lines diagonol
+ * @n: input variable
+ *
+ * Return: void
+ */
+
+void print_diagonol(int n)
+{
+        int i, j;
+	if (n <= 0)
+		_putchar('\n')
+			for (i = 0; i < n; i++)
+        {
+                _putchar(' ');
+        }
+	_putchar('\\')
+        _putchar('\n')
+}
